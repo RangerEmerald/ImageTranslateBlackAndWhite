a69e5bcd10f423140aaecba7f6479510ef26d89b
The Moon That Rises <58153205+RangerEmerald@users.noreply.github.com>
2021-08-19 21:29:20 -0600

    Fixed array size error

--- drawImageToByte.py
+++ drawImageToByte.py
@@ -10,15 +10,14 @@ imageName = None
 try:
     if len(sys.argv) < 2:
         raise ValueError("Could not find text file. Format: python3 [this file name] [textfile] [width] [height] <to save image name>(optional). Example: python3 drawImageToByte.py text.txt 200 200")
+    elif len(sys.argv) >= 2 and len(sys.argv) <= 3:
+        raise ValueError("Size array not right size. Array is [width, height]")
 
     if len(sys.argv) == 5:
         imageName = sys.argv[4]
 
     textFile = sys.argv[1]
     size = [int(sys.argv[2]), int(sys.argv[3])]
-    
-    if len(size) != 2:
-        raise ValueError("Size array not right size. Array is [width, height]")
 except Exception as error:
     print("An error has occured ---", error)
 else:
